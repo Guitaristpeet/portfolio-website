12dea31ce1b521da8dae27c36f7ca77ebea18b74
SU-T <guitaristpeet@gmail.com>
2022-07-25 06:38:23 -0700

    create projects.js

--- JS/main.js
+++ JS/main.js
@@ -80,17 +80,102 @@ gsap.to('.proj-star', {
     rotate: 90,
 });
 
-gsap.from('.contactInView', {
-    scrollTrigger: {
-        trigger: '.contactInView',
-        start: 'top 80%',
-        end: 'bottom 0%',
-        markers: true,
-        toggleActions: 'play none none reverse',
+// gsap.from('.contactInView', {
+//     scrollTrigger: {
+//         trigger: '.contactInView',
+//         start: 'top 80%',
+//         end: 'bottom 0%',
+//         // markers: true,
+//         toggleActions: 'play none none reverse',
+//     },
+//     stagger: {
+//         amount: 0.8
+//     },
+//     y: 35,
+//     opacity: 0,
+// });
+
+gsap.to('.arrow-down', {
+    y: 5,
+    repeat: -1,
+    yoyo: true,
+})
+
+// var text = document.querySelector('.scroll-down');
+// text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: true})
+//     .add({
+//         targets: '.scroll-down span',
+//         translateX: [0, -20],
+//         easing: 'linear',
+//         duration: 500,
+//         delay: anime.stagger(100),
+//     })
+//     .add({
+//         delay: 500,
+//     })
+//     .add({
+//         targets: '.scroll-down span',
+//         translateX: [-20, 0],
+//         easing: 'linear',
+//         duration: 500,
+//         delay: anime.stagger(100, {from: 'last'}),
+//     });
+
+const words = ['web development', 'innovation', 'technology', 'design', 'immersive experience', '3d', 'animation', 'game design', 'audio innovation', 'spatial audio', 'ambisonic mix'];
+const wordsAnim = document.querySelector('.words-animation');
+
+// words.forEach((word) => {
+//     let result = `<span class='letter'>${word}</span>`;
+//     wordsAnim.innerHTML = result;
+
+//     anime.timeline({
+//         target: wordsAnim,
+//         translateY: [0, -100],
+//         easing: 'linear',
+//         duration: 500,
+//         delay: 1000
+//     })
+
+// });
+// let allWords = words.map((word) => {
+//     return `<h3 class="words-animation contactInView">${word}</h3>`;
+// }).join('');
+// wordsAnim.innerHTML = allWords;
+
+
+
+
+
+// var text = document.querySelector('.words-animation');
+// text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: true})
+//     .add({
+//         targets: '.scroll-down span',
+//         translateX: [0, -20],
+//         easing: 'linear',
+//         duration: 500,
+//         delay: anime.stagger(100),
+//     })
+//     .add({
+//         delay: 500,
+//     })
+//     .add({
+//         targets: '.scroll-down span',
+//         translateX: [-20, 0],
+//         easing: 'linear',
+//         duration: 500,
+//         delay: anime.stagger(100, {from: 'last'}),
+//     });
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 1500
     },
-    stagger: {
-        amount: 0.8
-    },
-    y: 35,
-    opacity: 0,
+    direction  : 'vertical',
+    clickable: false,
+    allowTouchMove: false
 });
---
+++ JS/projects.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+        id: 4,
+        name: 'valla table',
+        desc: "Hell's Kitchen Must-Try Thai Restaurant",
+        info: '',
+        technologies: 'html / css / javascript',
+        img: '',
+    },
+    {
+        id: 3,
+        name: 'poveda',
+        desc: 'Bali Travel Agency Site',
+        info: '',
+        technologies: 'html / css / javascript',
+        img: '',
+    },
+    {
+        id: 2,
+        name: 'stornoway',
+        desc: 'construction company profile',
+        info: '',
+        technologies: 'html / css / javascript',
+        img: '',
+    },
+    {
+        id: 1,
+        name: 'real & true',
+        desc: "an artist's portfolio site",
+        info: '',
+        technologies: 'html / css / javascript',
+        img: '',
+    }
+]
